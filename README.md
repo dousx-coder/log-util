44092c6dedaa40f8b14d6366a1888f5ba916c55d
DouShaoxun <dousx_cn@163.com>
2022-06-05 19:41:44 +0800

    @Import注解

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ mvn clean package install -Dmaven.test.skip=true
 <dependency>
     <groupId>cn.cruder</groupId>
     <artifactId>log-util-spring-boot-starter</artifactId>
-    <version>1.0.0-SNAPSHOT</version>
+    <version>1.0.1-SNAPSHOT</version>
 </dependency>
 ```
+启动类添加`@EnableAopLog`注解打开日志功能
 
 配置:可自定义前后缀,以及日志输入级别
 
---
+++ log-util-spring-boot-autoconfigure/src/main/java/cn/cruder/logutil/annotation/EnableAopLog.java
@@ -0,0 +1,22 @@
+package cn.cruder.logutil.annotation;
+
+
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * 使用了@Import注解,取代从/META-INF/spring.factories加载配置
+ *
+ * @author dousx
+ * @date 2022-06-05 14:17
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@AutoConfigurationPackage
+@Import({cn.cruder.logutil.autoconfiguration.LogAutoConfiguration.class})
+public @interface EnableAopLog {
+}
